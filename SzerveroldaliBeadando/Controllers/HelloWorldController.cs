b6167dfa4dd4d16588557f66cf799a81bf37ca11
DaweY <vargadavidzsolt@gmail.com>
2024-10-03 10:25:43 +0200

    add a controller to an ASP.NET Core MVC app

---
+++ SzerveroldaliBeadando/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace SzerveroldaliBeadando.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Ez az index oldal.";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Szia {name}, ID: {ID}");
+        }
+    }
+}
