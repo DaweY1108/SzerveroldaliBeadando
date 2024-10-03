4e3742030ba932cb8fd94ad0d350c23ebc739bb7
DaweY <vargadavidzsolt@gmail.com>
2024-10-03 11:38:03 +0200

    add a new field to an ASP.NET Core MVC app

--- SzerveroldaliBeadando/Migrations/20241003093657_Rating.Designer.cs
+++ SzerveroldaliBeadando/Migrations/20241003093657_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SzerveroldaliBeadando.Data;
 
@@ -11,9 +12,11 @@ using SzerveroldaliBeadando.Data;
 namespace SzerveroldaliBeadando.Migrations
 {
     [DbContext(typeof(SzerveroldaliBeadandoContext))]
-    partial class SzerveroldaliBeadandoContextModelSnapshot : ModelSnapshot
+    [Migration("20241003093657_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
